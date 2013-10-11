13cd7c43c4b80a5ab4e09a84ebe1fd6cd59053c8
Chris Mueller <cmuellerrr@gmail.com>
2013-10-10 19:59:14 -0700

    added role and tools metadata added showing email address on click

---
+++ site/js/main.js
@@ -0,0 +1,5 @@
+//I realize this is a little wierd, but I really hate mailto
+//and this will help with spammers.
+var showEmail = function() {
+    document.getElementById('email').innerHTML="chris@iammueller.com";
+};

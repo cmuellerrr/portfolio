51e47203675dcda9ff8242c9b1528d4d43a5f06e
Chris Mueller <cmuellerrr@gmail.com>
2014-12-03 20:48:45 -0800

    some copy updates

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-//I realize this is a little wierd, but I really hate mailto
-//and this will help with spammers.
 window.onload = function() {
-    $('#email').click(function(event) {
-        $('#email').replaceWith("chris@iammueller.com");
-        event.preventDefault();
-    });
+    //nothing I guess...
 };

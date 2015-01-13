ddd09d630c749ce653118c166fb6066f73dc722d
Chris Mueller <cmuellerrr@gmail.com>
2015-01-12 20:45:46 -0800

    went with the permanent marker look. cut out some unworthy projects

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-window.onload = function() {
-    //nothing I guess...
+window.onload = function () {
+    var header = $('header');
+
+    if (header.length > 0) {
+        $(document).scroll(function () {
+            if ($(document).scrollTop() > 0) {
+                header.addClass('fixed');
+            }
+            else {
+                header.removeClass('fixed');
+            }
+        });
+    }
 };
